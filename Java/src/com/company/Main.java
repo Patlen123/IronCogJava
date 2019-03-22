e187b87876465b53dbb147568a5de1af6ecbdb2f
Patryk <patlen123@hotmail.com>
2019-03-22 23:05:28 +0000

    Directory Organisation

---
+++ Java/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Input the file you want to edit (or create a new one)");
+
+    }
+}
