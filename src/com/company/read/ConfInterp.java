53fcb0959698b13b5ba6994d1e297b848465b159
Michael <mishuk2000@gmail.com>
2019-04-19 19:12:28 +0100

    last commit :(

--- src/com/company/read/ConfInterp.java
+++ src/com/company/read/ConfInterp.java
@@ -1,7 +1,11 @@
+// I PUT AT LEAST A FEW HOURS INTO THIS SO I'M COMMITING,
+// THIS IS THE ONLY CLASS THAT IS EVEN CLOSE TO BEING READY
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
+
+
 
 // I am forced to leave this in and waste precious bytes.
 // 😊😊😊😊 LMAO WHO DID THIS 😊😊😊😊
@@ -11,7 +15,7 @@ public class ConfInterp{
 	// Scans a string that is predetermined to be of a colour. 
 	// If preparing a string, make sure it's at least 5 words long,
 	// otherwise it might be too short
-	private static ColorNode parseColor(String color){
+	private static Color parseColor(String color){
 		Scanner in = new Scanner(color);
 		String name  = in.next();
 		// Second element is always an "=", otherwise fail
@@ -69,7 +73,39 @@ public class ConfInterp{
 
 	}
 
-	private static Node nextParameter(String input){
+	// Reads in everything until a "}" has been scanned, returns
+	// all contents as a tree.
+	private static Tree parseTree(String input, String name){
+		Tree<Node> ouTtree = new Tree<Node>(name);
+		boolean reachedEndBracket = false;
+
+		while (!reachedEndBracket) {
+			Node newNode = nextNode(input);
+			// If nextNode returns null, it means that it scanned an
+			// end bracket.
+			if (newNode == null) {
+				reachedEndBracket = true;
+			} else {
+				returnTree.add(newNode);
+			}
+		}
+		return returnTree;
+	}
+
+	private static Building parseBuilding(String input){
+		// Splits everything around ';' into a string array
+		String[] p = input.split(";");
+		// Construct a new building out of the split parameters
+		return new Building(p[0], p[1], p[2], p[3], p[4],
+							p[5] p[6]);
+		
+	}
+		
+
+	// TODO idea: some parameters are initialized using n=e not n = e,
+	// instead of regex use String.split(), with a [ =] seperator so both
+	// are accounted for.
+	private static Node nextNode(String input){
 		Scanner in = new Scanner(input);
 		String[] element = new String[3];
 		element[0] = in.next();
@@ -77,122 +113,120 @@ public class ConfInterp{
 		// Number at least once, followed by an '=', '{', 
 		// and another number, a space and '}'.
 		if (element[0].matches("^[0-9]+={ *[0-9]+ *}$")) {
-			// make a TreeNode of that type
-			return returnTreeNode;
+
+			// Replaces everything after and including an '='
+			String name = input.replaceAll("=.*$", "");
+			// Replaces everything before and including an "{ ",
+			// then everything after and including an " }"
+			String data = input.replaceAll("^[0-9]*=\\{ *", "")
+				.replaceAll(" *\\}.*", "");
+			Number outNumber = new Number(name, data);
+
+			// Erase the matched string at the beggining
+			// You have to be specific, otherwise all '}' will be erased.
+			input = input.replaceAll("^[0-9]+={ *[0-9]+ *}[ \t]*", "");
+			return outNumber;
 		}
+
 		// Matches an ending bracket
 		if (element[0].matches("}")) {
-			// I have no idea how to communicate this,
-			// but think of something.
-			// Idea: make it so the only time that this returns null,
-			// the tree has ended and we need to jump out of it.
-			// Idea: if you see a {, call a recursive method and return
-			// the entire tree
+			input = input.replaceAll("[ \t]*//}[ \t]*", "");
+			// We communicate this by returning null.
 			return null;
 		}
 		
-		// color keyword indicates next strings will be a color
+		// "color" keyword indicates next strings will be a color
 		if (element[0].matches("color") ||
 			element[0].matches("color_ui")) {
 			String colorElement = element[0];
 			for (int i = 0; i < 6; i++) {
 				colorElement = " " + in.next();
 			}
+			// This looks retarded, but it works and prevents bugs later.
+			// Replaces everything that might match what an RGB/HSV
+			// color parameter may look like.
+			input = input
+				.replaceAll("^color[a-z0-9A-Z_]* *= *" +
+							"[a-z0-9A-Z]* *{ *[0-9.]+ [0-9.]+ [0-9.] *} *" +
+							"[ \t]*", "");
 			return parseColor(colorElement);
 		}
 
+		// num;word;num.num;*4num means it's a building
+		if (element[0]
+			.matches("^[0-9]+;[a-zA-Z_]+;([0-9]+\\.[0-9]{2};){4}[0-9]+$")) {
+
+			input.replaceAll("^[0-9]+;[a-zA-Z_]+;([0-9]+\\.[0-9]{2};){4}[0-9]+",
+							 "");
+			return parseBuilding(element[0]);
+		}
+							   
 
 		// Cannot analyze what to do based on element[0], continuing.
 		element[1] = in.next();
 		// If this is not an equals sign, the previous
 		// element was an option, therefore return it.
 		if (!element[1].matches("^=$")) {
-			// make an Option of element[0]
-			return returnTreeNode;	
+			
+			Option outOption = new Option(element[0]);
+			input = input.replaceAll("^[a-z0-9A-Z._]+", "");
+			return outOption;	
 		}
 
 		// Cannot analyze what to do based on element[1], continuing.
 		element[3] = in.next();
 		if (element[3].equals("{")) {
-			// haha recursion but much more complex:
-			// parseTree uses this method and recurviely calls itself
-			// if a tree is in change, uses null to find end of tree
-			parseTree(in);
+			// Remove everything at the beggining that might match a tree.
+			input = input.replaceAll("^[a-z0-9A-Z_]* *= *{[ \t]*", "");
+			// Return whatever parseTree outputs (SPOILER:a tree)
+			return parseTree(input, element[0]);
 		}
 
-		
-		
-
-		
-
-		
-		
+		// We have reached the final case, the regular "n = e"
+		Parameter out = new Parameter(element[0], element[3]);
+		return out;
 	}
 
-	
-	// Returns the paramater tree created from the file
-	public static MultiNode file(String file){
+
+	// Converts a file to string. Makes file a single string and
+	// increases I/O safety.
+	private static fileToString(String file){
 
 		Scanner in = null;
-		// The tree of the file
-		MultiNode fileTree =
-			// Root node id is the filename, filter to filename only
-			new MultiNode(file.replaceAll("^.*/", ""));
-		try{
+		String out = "";
+		try {
 			in = new Scanner(new File(file));
-			int lines = 0;
-			String currentLine = null;
-
-			while(in.hasNextLine()){
-				lines++;
-				currentLine = in.nextLine();
-				// Removes all commented out text
-				currentLine = currentLine.replaceAll("#.*$", "");
-				
-				Scanner lineIn = new Scanner(currentLine);
-				// Based on how many words are on a line
-				switch(wordsOnLine(currentLine)){
-				case 0:
-					// Empty line, can be safely ignored
-					break;
-				case 1:
-					// Can either be a tree closing bracket or an option
-					String opt = lineIn.next();
-					if (opt.equals("}")){
-						fileTree.leaveTree();
-					} else if (opt.matches("^[a-z0-9A-Z_.]*$")){
-						fileTree.appendOption(opt);
-					} else {
-						throw new InputMismatchException("error: line " + lines +
-														 "is incorrectly formatted.");
-					}
-					break;
-
-				case 3:
-					// Can either be a new tree or a variable declaration
-					String id = lineIn.next();
-					if (!lineIn.next().equals("=")){
-						throw new InputMismatchException("error: line " + lines
-														 +" incorrectly formatted");
-					}
-					String data = lineIn.next();
-					if (data.equals("{")){
-						fileTree.newTree(id);
-					} else {
-						fileTree.appendElement(id,data);
-					}
-					break;
-					
-				default:
-					// If none of the above, the file is incorrectly formatted.
-					throw new InputMismatchException("error: line " + lines +
-													 "is incorrectly formatted.");
-				}
-			
+			while (in.hasNext()) {
+				// Removes commented out text and appends to out
+				out += " " + in.next().replaceAll("#.*$", "");
 			}
 		}catch(FileNotFoundException e){
 			System.err.printf("error: file not found\n");
 			System.exit(1);
+		}
+
+		finally {
+			in.close();
+		}
+
+		return out;
+	}
+								
+	// Returns the paramater tree created from the file
+	public static Tree file(String file){
+		String fileString = fileToString(file);
+
+		// Root tree name is the filename, filter to filename only
+		Tree root = new Tree(file.replaceAll("^.*/", ""));
+
+		// While the fileString is not empty
+		while (!fileString.equals("")) {
+			// STRING IS PASS BY REFERENCE!
+			// You can erase what you don't need anymore.
+			// This method works by that principle.
+			root.add(nextNode(fileString));
+
+			
 		}
 		return fileTree;
 	}
